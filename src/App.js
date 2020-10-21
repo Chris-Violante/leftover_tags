50ad6e7799074342992d0016bebbe28f4f8ec5cd
Christopher Violante <c.violante211@gmail.com>
2020-10-20 22:34:21 -0600

    back end running

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 class App extends Component {
+  state = {
+    
+  }
   render() {
     return(
       <>
